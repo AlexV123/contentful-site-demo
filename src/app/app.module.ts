d6b94f3eae334a182abb159d434e0faf9801a29a
AlexVerzeaDeloitte <averzea@deloitte.ca>
2018-02-04 23:36:19 -0500

    Basic Contentful project done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes }   from '@angular/router';
 import { AppComponent } from './app.component';
+import { ContentfulService } from './contentful.service';
+// import new component
+import { ProductListComponent } from './product-list/product-list.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products',  component: ProductListComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // add product component
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [],
+  providers: [
+    ContentfulService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
